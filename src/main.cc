2d85cfe7b460ccbf15007dd29ff4fc3ea1b4a595
Noel Cower <ncower@gmail.com>
2013-06-11 12:38:44 -0700

    Add _controlfp calls to set FP precision/rounding.
    This is mostly to _attempt_ to ensure floating point arithmetic is the
    same across platforms. This probably doesn't actually matter in single-
    player games, but it should at least make it possible to later re-add
    net code (currently mostly stripped when using --no-server in the build
    options — would like to work on that again eventually).

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include "config.hh"
 #include "client/cl_main.hh"
 #include "sys_main.hh"
+#include <cfloat>
 
 
 static void nullify_log_callback()
@@ -12,6 +13,11 @@ static void nullify_log_callback()
 int main(int argc, char const *argv[])
 {
   using namespace snow;
+
+#ifdef S_USE_CONTROLFP
+  _controlfp(_PC_24, _MCW_PC);
+  _controlfp(_RC_NEAR, _MCW_RC);
+#endif
 
   std::atexit(nullify_log_callback);
   sys_init(argc, argv);
